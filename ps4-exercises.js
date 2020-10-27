0b837873852da1fe9e9ff08755f029691f549cf3
Nahuel <nahuelmartindacri@gmail.com>
2020-10-27 00:21:00 -0300

    PS4-004: exercise 4

--- ps4-exercises.js
+++ ps4-exercises.js
@@ -111,5 +111,35 @@ console.log(strMonths);
 var periodMayNov = months.slice(4, 11);
 console.log(periodMayNov);
 
+/* If Else */
+
+// Exercise 4-a
+
+var xy = Math.random();
+if(xy >= 0.5){
+    console.log('Greater than 0.5');
+}else{
+    console.log('Lower than 0.5');
+}
+
+// Exercise 4-b
+
+var Age = 26;
+if(Age < 2){
+    console.log('Bebé');
+}else if((Age > 1) && (Age < 12)){
+    console.log('Niño');
+}else if((Age > 11) && (Age < 19)){
+    console.log('Adolescente');
+}else if((Age > 18) && (Age < 30)){
+    console.log("Joven");
+}else if((Age > 29) && (Age < 60)){
+    console.log('Adulto');
+}else if((Age > 60) && (Age < 75)){
+    console.log('Adulto mayor');
+}else{
+    console.log("Anciano");
+}
+
 
 
