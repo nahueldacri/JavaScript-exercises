51897dc382a4afdb6dd57405bea3f5bb26ea7263
Nahuel <nahuelmartindacri@gmail.com>
2020-10-26 22:28:46 -0300

    PS4-001: exercise 1

---
+++ ps4-exercises.js
@@ -0,0 +1,25 @@
+/* Weekly problem exercises 4*/
+
+/* Variables and Operators */
+
+// Exercise 1-a
+
+var ageNahuel = 26;
+var ageCamila = 25;
+var sumOfAges = ageCamila + ageNahuel;
+console.log('Sum of ages: ' + sumOfAges);
+
+// Exercise 1-b
+
+var users = 'Cristian and Rodrigo';
+var action = 'play a tennis match.';
+var sentence = users + action; 
+console.log(sentence);
+
+//Exercise 1-c
+
+var individualSports = 'Tennis, Swimming, Golf';
+var groupSports = 'Football, Basketball, Rugby';
+var totalLength = individualSports.length + groupSports.length;
+console.log('Total length of variables: ' + totalLength);
+
